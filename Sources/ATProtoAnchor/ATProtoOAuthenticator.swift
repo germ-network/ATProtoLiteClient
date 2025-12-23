b9ba18dd449af560faf184aa7a268d2fccdda3f1
Mark Xue <mark@germ.network>
2025-12-22 22:57:30 -0800

    remove hacky initial capture

--- Sources/ATProtoAnchor/ATProtoOAuthenticator.swift
+++ Sources/ATProtoAnchor/ATProtoOAuthenticator.swift
@@ -83,39 +83,6 @@ public struct ATProtoOAuthenticator: Sendable {
 // Authentication - capture initial authentication and pass-through to
 // authenticate
 extension ATProtoOAuthenticator {
-	//simple way to prevent concurrent writes and reads and isolate login var
-	//Caller will be on main thread anyway
-	@MainActor
-	static public func captureInitialLogin(
-		handle: String,
-		pdsUrl: URL,
-		dpopSigner: @escaping DPoPSigner.JWTGenerator,
-	) async throws -> Login {
-		var login: Login?
-		let authenticator = try await ATProtoOAuthenticator(
-			handleOrDid: handle,
-			pdsURL: pdsUrl,
-			dpopSigner: dpopSigner,
-			loginStorage: .init(
-				retrieveLogin: { @MainActor in
-					login
-				},
-				storeLogin: { @MainActor newLogin in
-					login = newLogin
-				}
-			)
-		)
-
-		//TODO: when we change upstream to return the authenticate
-		try await authenticator.authenticator.authenticate()
-
-		guard let login else {
-			throw ATProtoClientError.missingLogin
-		}
-
-		return login
-	}
-
 	public func authenticate() async throws {
 		try await authenticator.authenticate()
 	}
