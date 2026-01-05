abce67571a241ac2c40955eb11da100249f98990
Anna M <anna@germ.network>
2026-01-05 14:14:06 -0800

    Rev ATResolve/OAuthenticator

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
 		),
 		.package(
 			url: "https://github.com/germ-network/OAuthenticator",
-			from: "0.7.1"
+			exact: "0.7.1-germ.1"
 		),
 		.package(
 			url: "https://github.com/germ-network/ATResolve",
-			from: "1.1.1"
+			from: "1.1.3"
 		),
 		.package(url: "https://github.com/vapor/jwt-kit.git", from: "5.0.0"),
 	],
